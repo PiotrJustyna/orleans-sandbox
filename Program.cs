8c6c9589e4fa6226433c4ce24d370e2283d1bc8e
Piotr Justyna <piotr.justyna@gmail.com>
2020-04-27 09:59:12 +0000

    Hello world code + .vscode directory.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace orleans_sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
