a35701325308159febc5c10dd27f48771429dd58
Piotr Justyna <piotr.justyna@gmail.com>
2020-04-28 09:59:17 +0000

    Prepared the code to run a hello world example.

--- host/Program.cs
+++ host/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace OrleansSandbox.Host
 {
-    class Program
+  class Program
+  {
+    static async Task<int> Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(OrleansSandbox.Lib.Lib.Greet());
-        }
+      try
+      {
+        var host = await StartSilo();
+        Console.WriteLine("Press Enter to terminate...");
+        Console.ReadLine();
+
+        await host.StopAsync();
+
+        return 0;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex);
+        return 1;
+      }
     }
+
+    private static async Task<ISiloHost> StartSilo()
+    {
+      var builder = new SiloHostBuilder()
+          .UseLocalhostClustering()
+          .Configure<ClusterOptions>(options =>
+          {
+            options.ClusterId = "dev";
+            options.ServiceId = "HelloWorldApp";
+          })
+          .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+          .ConfigureLogging(logging => logging.AddConsole());
+
+      var host = builder.Build();
+      await host.StartAsync();
+      return host;
+    }
+  }
 }
